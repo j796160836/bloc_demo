6c5911e7ca51a7d0be3b75c086594b8ca2a628bc
Johnny <>
2022-11-26 10:49:44 +0800

    clean up code

--- lib/my_bloc.dart
+++ lib/my_bloc.dart
@@ -3,6 +3,7 @@ import 'package:bloc_demo/my_bloc_state.dart';
 import 'package:bloc_demo/score_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+// bloc 寫法
 class MyBloc extends Bloc<MyBlocEvent, MyBlocState> {
   ScoreRepositoryInterface scoreRepo;
 
@@ -15,6 +16,26 @@ class MyBloc extends Bloc<MyBlocEvent, MyBlocState> {
       } else {
         emit(CryingState());
       }
+    });
+  }
+}
+
+// Cubit 寫法
+class MyCubit extends Cubit<MyBlocState> {
+  ScoreRepositoryInterface scoreRepo;
+
+  MyCubit({required this.scoreRepo}) : super(InitState()) {}
+
+  void getScoringData() {
+    emit(InitState());
+    scoreRepo.getScore().then((score) {
+      if (score >= 60) {
+        emit(SmileState());
+      } else {
+        emit(CryingState());
+      }
+    }).catchError((error) {
+      emit(ErrorState(error));
     });
   }
 }
